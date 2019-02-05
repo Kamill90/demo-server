67000d2550ba4665e6c340a9a2db4bd28dd56379
don kamillo <kamil2912@gmail.com>
2019-02-05 23:10:29 +0100

    pred env

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const { Prisma } = require('prisma-binding');
 const { resolvers } = require('./resolvers');
 
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'https://demo-server-db-fb6ed0d759.herokuapp.com/prisma-demo-server-db/dev',
-  secret: 'qwe123gdfs324',
-  debug: false,
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: 'https://demo-server-db-fb6ed0d759.herokuapp.com/prisma-demo-server-db/dev',
+    secret: 'qwe123gdfs324',
+    debug: false,
 });
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
-  resolvers,
-  context: req => ({
-    ...req,
-    prisma,
-  }),
+    typeDefs: 'src/schema.graphql',
+    resolvers,
+    context: req => ({
+        ...req,
+        prisma,
+    }),
 });
 
 server.start(() => console.log('Server is up'));
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,19 +1,19 @@
 const getUserId = require('../utils');
 
 const Query = {
-  getAllUsers(root, args, { prisma }, info) {
-    return prisma.query.users(null, info);
-  },
-  async getMe(root, args, { prisma, request }, info) {
-    const userId = getUserId(request);
-    const user = await prisma.query.user({
-      where: {
-        id: userId,
-      },
-    }, info);
+    getAllUsers(root, args, { prisma }, info) {
+        return prisma.query.users(null, info);
+    },
+    async getMe(root, args, { prisma, request }, info) {
+        const userId = getUserId(request);
+        const user = await prisma.query.user({
+            where: {
+                id: userId,
+            },
+        }, info);
 
-    return user;
-  },
+        return user;
+    },
 };
 
 module.exports = { Query };
