694e449b3032a5f0c3647cea505554ac1361f10b
don kamillo <kamil2912@gmail.com>
2019-02-10 15:08:05 +0100

    git ignore

--- dist/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Query_1 = require("./Query");
-var Mutation_1 = require("./Mutation");
-var resolvers = {
-    Query: Query_1.Query,
-    Mutation: Mutation_1.Mutation,
-};
-exports.resolvers = resolvers;
